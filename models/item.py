c8231a2de0e6bb2a8960fd213f6b2cfcafb3d957
pele98 <pekka.lehtola98@gmail.com>
2020-11-20 12:17:24 +0200

    models/item resources/item Both started

--- models/item.py
+++ models/item.py
@@ -8,11 +8,25 @@ def get_last_id():
     return last_item.id + 1
 
 class Item:
-    def __init__(self, name, description, img, rating, ratings, price, amount):
+    def __init__(self, name, description, tags, rating, ratings, price, amount):
         self.id = get_last_id()
         self.name = name
         self.description = description
+        self.tags = tags
         self.rating = 0
         self.ratings = 0
         self.price = price
         self.amount = amount
+
+    @property
+    def data(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "tags": self.tags,
+            "rating": self.rating,
+            "ratings": self.ratings,
+            "price": self.price,
+            "amount": self.amount
+        }
