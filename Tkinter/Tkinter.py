ef0a2489e48bf43e0c10da9b68a5d6cb60136a68
mikko223 <mikkolehtola@yahoo.fi>
2020-11-20 19:48:25 +0200

    login screen tehty ja koti sivu aloitettu

--- Tkinter/Tkinter.py
+++ Tkinter/Tkinter.py
@@ -1,11 +1,31 @@
-import tkinter
 from tkinter import *
-Win = Tk()
-Win.geometry("1000x600")
-def eexit():
-    Win.destroy()
-    exit()
-testbutton = Button(Win,text="exit",width=10,command=eexit,bg="red")
-testbutton.place(y = 500, x = 300)
+Login = Tk()
+Login.geometry("500x300")
+Login.title("Login")
+Label(text="login credetials",width="100",height="5",bg="white").pack()
 
-Win.mainloop()
+def Main():
+    Mainmenu = Toplevel(Login)
+    Mainmenu.title("Home")
+    Mainmenu.geometry("600x400")
+
+
+usernameLabel= Label(text="Username").pack()
+username = StringVar()
+usernameEntry =Entry (Login,textvariable=username).pack()
+
+
+passwordLabel= Label(text="password").pack()
+password = StringVar()
+passwordEntry = Entry(Login,textvariable=password).pack()
+Label(Login,text="").pack()
+button = Button(Login,text="login",width=10,command=Main ,bg="red").pack(pady=0)
+button1 = Button(Login,text="exit",width=10,command=exit,bg="red").pack(pady=0)
+
+
+
+def exit():
+    Login.destroy()
+    exit()
+
+Login.mainloop()
