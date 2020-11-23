6fd5681f30a53c5fa451e7d80244b4f1a74f8728
mikko223 <mikkolehtola@yahoo.fi>
2020-11-23 14:02:15 +0200

    Login screen and home screen fixedish

--- Tkinter/Tkinter.py
+++ Tkinter/Tkinter.py
@@ -1,31 +1,88 @@
-from tkinter import *
-Login = Tk()
-Login.geometry("500x300")
-Login.title("Login")
-Label(text="login credetials",width="100",height="5",bg="white").pack()
-
-def Main():
-    Mainmenu = Toplevel(Login)
-    Mainmenu.title("Home")
-    Mainmenu.geometry("600x400")
+import tkinter as tk
 
 
-usernameLabel= Label(text="Username").pack()
-username = StringVar()
-usernameEntry =Entry (Login,textvariable=username).pack()
-
-
-passwordLabel= Label(text="password").pack()
-password = StringVar()
-passwordEntry = Entry(Login,textvariable=password).pack()
-Label(Login,text="").pack()
-button = Button(Login,text="login",width=10,command=Main ,bg="red").pack(pady=0)
-button1 = Button(Login,text="exit",width=10,command=exit,bg="red").pack(pady=0)
-
-
+LARGE_FONT= ("Verdana", 12)
 
 def exit():
-    Login.destroy()
-    exit()
+    Main.destroy()
 
-Login.mainloop()
+class Main(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+
+        container.pack(side="top", fill="both", expand = True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (StartPage, PageOne, PageTwo):
+
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self,parent)
+        label = tk.Label(self, text="Login", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button = tk.Button(self, text="Login",
+                           command=lambda: controller.show_frame(PageOne))
+        button.pack()
+
+        button2 = tk.Button(self, text="Exit",command=exit)
+        button2.pack()
+
+
+class PageOne(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Page One!!!", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button1 = tk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        button2 = tk.Button(self, text="Page Two",
+                            command=lambda: controller.show_frame(PageTwo))
+        button2.pack()
+
+
+class PageTwo(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Page Two!!!", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button1 = tk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        button2 = tk.Button(self, text="Page One",
+                            command=lambda: controller.show_frame(PageOne))
+        button2.pack()
+
+
+
+app = Main()
+app.mainloop()
