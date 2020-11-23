f97e472bc1b749ff22a2f6920ef30d4ca49dbcd9
pele98 <pekka.lehtola98@gmail.com>
2020-11-23 13:26:01 +0200

    Importing item resources to app.py

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
 from flask import Flask
 from flask_restful import Api
 from flask_migrate import Migrate
-# from config import (Config)
-# from resources.item import (all resources)
+from config import Config
+from resources.item import ItemListResource, ItemResource
 # from resources.user import (all resources)
 # from resources.token import (all resources, black_list)
-# from extensions import jwt, db
+from extensions import jwt, db
 
 def create_app():
     app = Flask(__name__)
-    # app.config.from_object(Config)
+    app.config.from_object(Config)
     register_extensions(app)
-    # register_resources(app)
+    register_resources(app)
     return app
 def register_extensions(app):
-    # db.init_app(app)
-    # migrate = Migrate(app, db)
-    # jwt.init_app(app)
-    # @jwt.token_in_blacklist_loader
+    db.init_app(app)
+    migrate = Migrate(app, db)
+    jwt.init_app(app)
+
+    @jwt.token_in_blacklist_loader
     def check_if_token_in_blacklist(decrypted_token):
         jti = decrypted_token["jti"]
-        # return jti in black_list
+        return jti in black_list
 
 def register_resources(app):
     api = Api(app)
@@ -28,8 +29,8 @@ def register_resources(app):
     # api.add_resource(UserResource, "/users/<string:username>")
     # api.add_resource(UserItemListResource, "/users/<string:username>/items")
     # api.add_resource(MeResource, "/me")
-    # api.add_resource(ItemListResource, "/items")
-    # api.add_resource(ItemResource, "/items/<int:item_id>")
+    api.add_resource(ItemListResource, "/items")
+    api.add_resource(ItemResource, "/items/<int:item_id>")
     # api.add_resource(ItemRatingsResource, "/items/<int:item_id>/ratings")
 
 if __name__ == "__main__":
--- resources/item.py
+++ resources/item.py
@@ -35,102 +35,102 @@ class ItemListResource(Resource):
 
         return item_schema.dump(item).data, HTTPStatus.CREATED
 
-    class ItemResource(Resource):
+class ItemResource(Resource):
 
-        @jwt_required
-        def patch(self, item_id):
+    @jwt_required
+    def patch(self, item_id):
 
-            json_data = request.get_json()
+        json_data = request.get_json()
 
-            data, errors = item_schema.load(data=json_data, partial=("name",))
+        data, errors = item_schema.load(data=json_data, partial=("name",))
 
-            if errors:
-                return {"message": "Validation errors", "errors": errors}, HTTPStatus.BAD_REQUEST
+        if errors:
+            return {"message": "Validation errors", "errors": errors}, HTTPStatus.BAD_REQUEST
 
-            item = Item.get_by_id(item_id=item_id)
+        item = Item.get_by_id(item_id=item_id)
 
-            if item is None:
-                return {"message": "item not found"}, HTTPStatus.NOT_FOUND
+        if item is None:
+            return {"message": "item not found"}, HTTPStatus.NOT_FOUND
 
-            current_user = get_jwt_identity()
+        current_user = get_jwt_identity()
 
-            if current_user != item.user_id:
-                return {"message": "Access is not allowed"}, HTTPStatus.FORBIDDEN
+        if current_user != item.user_id:
+            return {"message": "Access is not allowed"}, HTTPStatus.FORBIDDEN
 
-            item.name = data.get("name") or item.name
-            item.description = data.get("description") or item.description
-            item.tags = data.get("tags") or item.tags
-            item.ratings = data.get("ratings") or item.ratings
-            item.rating = data.get("rating") or item.rating
-            item.price = data.get("price") or item.price
-            item.amount = data.get("amount") or item.amount
+        item.name = data.get("name") or item.name
+        item.description = data.get("description") or item.description
+        item.tags = data.get("tags") or item.tags
+        item.ratings = data.get("ratings") or item.ratings
+        item.rating = data.get("rating") or item.rating
+        item.price = data.get("price") or item.price
+        item.amount = data.get("amount") or item.amount
 
-            item.save()
+        item.save()
 
-            return item_schema.dump(item).data, HTTPStatus.OK
+        return item_schema.dump(item).data, HTTPStatus.OK
 
-        @jwt_optional
-        def get(self, item_id):
+    @jwt_optional
+    def get(self, item_id):
 
-            item = Item.get_by_id(item_id=item_id)
+        item = Item.get_by_id(item_id=item_id)
 
-            if item is None:
-                return {"message": "Item not found"}, HTTPStatus.NOT_FOUND
+        if item is None:
+            return {"message": "Item not found"}, HTTPStatus.NOT_FOUND
 
-            current_user = get_jwt_identity()
+        current_user = get_jwt_identity()
 
-            return item.data(), HTTPStatus.OK
+        return item.data(), HTTPStatus.OK
 
-        @jwt_optional
-        def get(self, tags):
+    @jwt_optional
+    def get(self, tags):
 
-            tags = Item.get_by_tags(tags=tags)
+        tags = Item.get_by_tags(tags=tags)
 
-            if tags is None:
-                return {"message": "Items not found with this tag"}, HTTPStatus.NOT_FOUND
+        if tags is None:
+            return {"message": "Items not found with this tag"}, HTTPStatus.NOT_FOUND
 
-            return tags.data(), HTTPStatus.OK
+        return tags.data(), HTTPStatus.OK
 
-        @jwt_required
-        def put(self, item_id):
+    @jwt_required
+    def put(self, item_id):
 
-            json_data = request.get_json()
+        json_data = request.get_json()
 
-            item = Item.get_by_id(item_id=item_id)
+        item = Item.get_by_id(item_id=item_id)
 
-            if item is None:
-                return {"message": "Item not found"}, HTTPStatus.NOT_FOUND
+        if item is None:
+            return {"message": "Item not found"}, HTTPStatus.NOT_FOUND
 
-            current_user = get_jwt_identity()
+        current_user = get_jwt_identity()
 
-            if current_user != item.user_id:
-                return {"message": "Access is not allowed"}, HTTPStatus.FORBIDDEN
+        if current_user != item.user_id:
+            return {"message": "Access is not allowed"}, HTTPStatus.FORBIDDEN
 
-            item.name = json_data["name"]
-            item.description = json_data["description"]
-            item.tags = json_data["tags"]
-            item.ratings = json_data["ratings"]
-            item.rating = json_data["rating"]
-            item.price = json_data["price"]
-            item.amount = json_data["amount"]
+        item.name = json_data["name"]
+        item.description = json_data["description"]
+        item.tags = json_data["tags"]
+        item.ratings = json_data["ratings"]
+        item.rating = json_data["rating"]
+        item.price = json_data["price"]
+        item.amount = json_data["amount"]
 
-            item.save()
+        item.save()
 
-            return item.data, HTTPStatus.OK
+        return item.data, HTTPStatus.OK
 
-        @jwt_required
-        def delete(self, item_id):
+    @jwt_required
+    def delete(self, item_id):
 
-            item = Item.get_by_id(item_id=item_id)
+        item = Item.get_by_id(item_id=item_id)
 
-            if item is None:
-                return {"message": "Item not found"}, HTTPStatus.NOT_FOUND
+        if item is None:
+            return {"message": "Item not found"}, HTTPStatus.NOT_FOUND
 
-            current_user = get_jwt_identity()
+        current_user = get_jwt_identity()
 
-            if current_user != item.user_id:
-                return {"message": "Access is not allowed"}, HTTPStatus.FORBIDDEN
+        if current_user != item.user_id:
+            return {"message": "Access is not allowed"}, HTTPStatus.FORBIDDEN
 
-            item.delete()
+        item.delete()
 
-            return {"message": "Item deleted"}, HTTPStatus.NO_CONTENT
+        return {"message": "Item deleted"}, HTTPStatus.NO_CONTENT
