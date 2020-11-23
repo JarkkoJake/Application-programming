153188acfafd533c948621df77f25eb2663d373c
developer <JarkkoJake>
2020-11-23 12:51:49 +0200

    models/user alotettu

--- models/user.py
+++ models/user.py
@@ -1,27 +1,33 @@
-from models.item import Item
+from extensions import db
 
-user_list = []
-def next_id():
-    if len(user_list == 0):
-        return 1
-    return len(user_list)+1
+class User(db.Model):
+    __tablename__ = "user"
 
-def calc_rating(items):
-    sum = 0
-    n = 0
-    for i in items:
-        sum = i.rating
-        n += 1
-    return sum/n
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False)
+    items = db.relationship("item", backref="user")
+    rating = db.Column(db.Float())
+    profile_picture = db.Column(db.String(300))
+    is_admin = db.Column(db.Boolean(), default=False)
+    created_at = db.Column(db.DateTime(), nullable=False,
+                           server_deafault=db.func.now())
+    updated_at = db.Column(db.DateTime(), nullable=False,
+                           server_deafault=db.func.now(),
+                           onupdate=db.func.now())
 
-class User:
-    def __init__(self, data):
-        self.id = next_id()
-        self.username = data["name"]
-        self.email = data["email"]
-        self.password = data["password"]
-        self.items = []
-        self.rating = calc_rating(self.items)
-        self.profile_picture = None
-        self.is_admin = False
-        user_list.append(self)
+    @classmethod
+    def get_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
+    @classmethod
+    def get_by_email(cls, email):
+        return cls.query.filter_by(email=email).first()
+    @classmethod
+    def get_by_id(cls, id):
+        return cls.query.filter_by(id=id).first()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
