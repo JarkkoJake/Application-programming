22ec67d9602a13c2cb0e56f568bf3f0158ab7dc9
Teemu <teemu.tahtinen@edu.turkuamk.fi>
2020-12-03 14:29:45 +0200

    item rating lisätty

--- models/rating.py
+++ models/rating.py
@@ -1,4 +1,6 @@
 from extensions import db
+from models.item import Item
+from models.user import User
 
 
 class Rating(db.Model):
@@ -15,15 +17,21 @@ class Rating(db.Model):
 
     @classmethod
     def get_by_item(cls, item_id):
-        return cls.query.filter_by(item_id=item_id).first()
+        return cls.query.filter_by(item_id=item_id).all()
 
     @classmethod
     def get_by_user(cls, user_id):
-        return cls.query.filter_by(user_id=user_id).first()
+        return cls.query.filter_by(user_id=user_id).all()
 
     @classmethod
     def get_all(cls):
         return cls.query.filter_by().all()
+
+    @classmethod
+    def get_by_user_item(cls, user_id, item_id):
+        return cls.query.filter_by(user_id=user_id, item_id=item_id).first()
+
+
 
     def save(self):
         db.session.add(self)
