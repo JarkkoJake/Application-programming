3563c621a80b44a174b848cc66b0e101c66ac5b0
Teemu <teemu.tahtinen@edu.turkuamk.fi>
2020-12-11 09:40:35 +0200

    rating juttui

--- resources/rating.py
+++ resources/rating.py
@@ -37,36 +37,38 @@ class RatingListResource(Resource):
         rating.user_id = current_user
         rating.item_id = item.id
         rating.save()
+
+        update_ratings(item_id=item.id, current_user=current_user)
 ###
-        ratings = Rating.get_by_item(item_id=item_id)
-        i_rating = 0.0
-        r = 0
-        for rating in ratings:
-            r = r + 1
-            i_rating = i_rating + rating.__dict__["rating"]
-
-        i_rating_dict = {"rating": i_rating / r}
-
-
-        item.rating = i_rating_dict.get("rating")
-
-
-        item.save()
+#        ratings = Rating.get_by_item(item_id=item_id)
+#        i_rating = 0.0
+#        r = 0
+#        for rating in ratings:
+#            r = r + 1
+#            i_rating = i_rating + rating.__dict__["rating"]
+#
+#        i_rating_dict = {"rating": i_rating / r}
+#
+#
+#        item.rating = i_rating_dict.get("rating")
+#
+#
+#        item.save()
 ###
-        user = User.get_by_id(id = current_user)
-        user_items = Rating.get_by_user(user_id=item.user_id)
-        u_rating = 0.0
-        r = 0
-
-        for rating in user_items:
-            r = r + 1
-            u_rating = u_rating + rating.__dict__["rating"]
-
-        u_rating_dict = {"rating": u_rating / r}
-
-        user.rating = u_rating_dict.get("rating")
-
-        user.save()
+#        user = User.get_by_id(id = current_user)
+#        user_items = Rating.get_by_user(user_id=item.user_id)
+#        u_rating = 0.0
+#        r = 0
+#
+#        for rating in user_items:
+#            r = r + 1
+#            u_rating = u_rating + rating.__dict__["rating"]
+#
+#        u_rating_dict = {"rating": u_rating / r}
+#
+#        user.rating = u_rating_dict.get("rating")
+#
+#        user.save()
 
 
 
@@ -97,45 +99,47 @@ class RatingListResource(Resource):
         rating.rating_text = data.get("rating_text") or rating.rating_text
 
         rating.save()
+
+        update_ratings(item_id=item.id, current_user=current_user)
 ####
-        ratings = Rating.get_by_item(item_id=item_id)
-        i_rating = 0.0
-        r = 0
-        for rating in ratings:
-            r = r + 1
-            i_rating = i_rating + rating.__dict__["rating"]
-
-        i_rating_dict = {"rating": i_rating / r}
-
-        item.name = item.name
-        item.description = item.description
-        item.tags = item.tags
-        item.ratings = item.ratings
-        item.rating = i_rating_dict.get("rating")
-        item.price = item.price
-        item.amount = item.amount
-
-        item.save()
+#        ratings = Rating.get_by_item(item_id=item_id)
+#        i_rating = 0.0
+#        r = 0
+#        for rating in ratings:
+#            r = r + 1
+#            i_rating = i_rating + rating.__dict__["rating"]
+#
+#        i_rating_dict = {"rating": i_rating / r}
+#
+#        item.name = item.name
+#        item.description = item.description
+#        item.tags = item.tags
+#        item.ratings = item.ratings
+#        item.rating = i_rating_dict.get("rating")
+#        item.price = item.price
+#        item.amount = item.amount
+#
+#        item.save()
 ####
-        user = User.get_by_id(id=current_user)
-        user_items = Rating.get_by_user(user_id=item.user_id)
-        u_rating = 0.0
-        r = 0
-
-        for rating in user_items:
-            r = r + 1
-            u_rating = u_rating + rating.__dict__["rating"]
-
-        u_rating_dict = {"rating": u_rating / r}
-
-        user.rating = u_rating_dict.get("rating")
-
-        user.save()
+#        user = User.get_by_id(id=current_user)
+#        user_items = Rating.get_by_user(user_id=item.user_id)
+#        u_rating = 0.0
+#        r = 0
+#
+#        for rating in user_items:
+#            r = r + 1
+#            u_rating = u_rating + rating.__dict__["rating"]
+#
+#        u_rating_dict = {"rating": u_rating / r}
+#
+#        user.rating = u_rating_dict.get("rating")
+#
+#        user.save()
 
         return rating_schema.dump(rating).data, HTTPStatus.OK
 
     @jwt_required
-    def put(self,item_id):
+    def put(self, item_id):
         ratings = Rating.get_by_item(item_id=item_id)
         i_rating = 0.0
         r = 0
@@ -168,3 +172,31 @@ class RatingListResource(Resource):
         return i_rating / r
 
 
+def update_ratings(item_id, current_user):
+    item = Item.get_by_id(item_id=item_id)
+    ratings = Rating.get_by_item(item_id=item_id)
+    i_rating = 0.0
+    r = 0
+    for rating in ratings:
+        r = r + 1
+        i_rating = i_rating + rating.__dict__["rating"]
+    i_rating_dict = {"rating": i_rating / r}
+
+    item.rating = i_rating_dict.get("rating")
+
+    item.save()
+
+    user = User.get_by_id(id=current_user)
+    user_items = Rating.get_by_user(user_id=item.user_id)
+    u_rating = 0.0
+    r = 0
+
+    for rating in user_items:
+        r = r + 1
+        u_rating = u_rating + rating.__dict__["rating"]
+
+    u_rating_dict = {"rating": u_rating / r}
+
+    user.rating = u_rating_dict.get("rating")
+
+    user.save()
